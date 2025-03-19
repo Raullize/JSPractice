4efcdb21b4bfc6211ddfa71c4975e29232095c5c
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-03-18 21:20:56 -0300

    :bulb: docs: padroniza comentários na linguagem em inglês

--- Desafios Praticos Scholarship OpenFinance/Ex2/script.js
+++ Desafios Praticos Scholarship OpenFinance/Ex2/script.js
@@ -8,8 +8,8 @@ function checkPrimeNumber(number) {
     }
 
     for (let i = 2; i <= Math.sqrt(number); i++) {
-    // O uso de Math.sqrt(number) é uma otimização da aplicação, pois se um número não é primo, ele terá
-    // pelo menos um divisor neste intervalo, permitindo uma verificação mais eficiente
+    // The use of Math.sqrt(number) is an optimization of the application because if a number is not prime, 
+    // it will have at least one divisor within this range, allowing for a more efficient verification.
         if (number % i === 0) {
             return false;
         }
